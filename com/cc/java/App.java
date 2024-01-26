49728b6ae6f1b63a67a2ec8f6774860b7cb02e7e
J1991Ha <jasper.haag@icloud.com>
2024-01-26 10:46:48 +0100

    -switch short -fix get Age

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,26 +1,25 @@
 package com.cc.java;
 
 public class App {
-    
+
     public static void main(String[] args) {
 
-        Cat cat = new Cat("Grizabella", "white", 29);
+        Cat cat = new Cat("Grizabella", "white", 29, true);
 
         output(cat.getStringAttributes("#name"));
         output(cat.getStringAttributes("#color"));
 
-        output(cat.getAge());  
-        
+        output(cat.getAge());
 
 
         output("-------------------------");
 
-        Cat cat2 = new Cat("Alonzo", "grey", 35);
+        Cat cat2 = new Cat("Alonzo", "grey", 35, false);
 
         output(cat2.getStringAttributes("#name"));
         output(cat2.getStringAttributes("#color"));
 
-        output(cat2.getAge()); 
+        output(cat2.getAge());
 
     }
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,31 +2,34 @@ package com.cc.java;
 
 public class Cat {
 
-  private String name;
-  private String furColor;
-  private int age; 
+    private final String name;
+    private final String furColor;
+    private final int age;
+    private final boolean isFemale;
 
 
-public Cat(String name, String furColor, int age) {
-    this.name = name;
-    this.furColor = furColor;
-    this.age = age;
-  }
+    public Cat(String name, String furColor, int age, boolean isFemale) {
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+        this.isFemale = isFemale;
+    }
 
-public String getStringAttributes(String op) {
-  switch (op) {
-    case "#name":
-      return name;
-    case "#color":
-      return furColor;
-    default:
-      return "ERROR!";
-  }
-}
+    public String getStringAttributes(String op) {
+        return switch (op) {
+            case "#name" -> name;
+            case "#color" -> furColor;
+            default -> "ERROR!";
+        };
+    }
 
-public String getAge() {
-    return String.valueOf(age);
-  }
+    public String getAge() {
+        if (isFemale) {
+            return "This is an inappropriate question!";
+        } else {
+            return String.valueOf(age);
+        }
+    }
 
 
 }
